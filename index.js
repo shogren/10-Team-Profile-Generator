6ef037f78104c783cd40b1bebe952767696a6104
jon-shogren <jon.shogren@mailchimp.com>
2022-09-12 11:30:04 -0400

    main functionality and styling complete, outputs file successfully

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
-const path = require("path");
 const fs = require("fs");
+const createTeam = require("./src/template.js")
 
 const Manager = require("./lib/Manager.js");
 const Engineer = require("./lib/Engineer.js");
@@ -10,63 +10,62 @@ team = [];
 
 function run () {
 
-    function buildTeam () {
-      inquirer.prompt([{
-        type: "list",
-        message: "Let's add a new Employee! What is their role?",
-        name: "employee",
-        choices: ["Manager", "Engineer", "Intern", "No more team members are needed."]
-      }]).then(function (userInput) {
-        switch(userInput.employee) {
-          case "Manager":
-            manager();
-            break;
-          case "Engineer":
-            engineer();
-            break;
-          case "Intern":
-            intern();
-            break;
-          default:
-            htmlBuilder();
-        }
-      })
-    }
-}
-
-
-function manager() {
-    inquirer.prompt ([
-      
-      {
-        type: "input",
-        name: "managerName",
-        message: "What is the manager's name?"
-      },
-  
-      {
-        type: "input",
-        name: "managerId",
-        message: "What is the manager's employee ID number?"
-      },
-  
-      {
-        type: "input",
-        name: "managerEmail",
-        message: "What is the manager's email address?"
-      },
-  
-      {
-        type: "input",
-        name: "managerNumber",
-        message: "What is the manager's office number?"
+  function buildTeam () {
+    inquirer.prompt([{
+      type: "list",
+      message: "Let's add a new Employee! What is their role?",
+      name: "employee",
+      choices: ["Manager", "Engineer", "Intern", "Done."]
+    }]).then(function (userInput) {
+      switch(userInput.employee) {
+        case "Manager":
+          manager();
+          break;
+        case "Engineer":
+          engineer();
+          break;
+        case "Intern":
+          intern();
+          break;
+        default:
+          buildSite();
       }
-  
-    ]).then(data => {
-      const manager = new Manager(data.managerName, data.managerId, data.managerEmail, data.managerNumber);
-      team.push(manager);
-      buildTeam();
-    });
+    })
+  }
+
+  function manager() {
+      inquirer.prompt ([
+        
+        {
+          type: "input",
+          name: "managerName",
+          message: "What is the manager's name?"
+        },
+    
+        {
+          type: "input",
+          name: "managerId",
+          message: "What is the manager's ID number?"
+        },
+    
+        {
+          type: "input",
+          name: "managerEmail",
+          message: "What is the manager's email address?"
+        },
+    
+        {
+          type: "input",
+          name: "managerNumber",
+          message: "What is the manager's office number?"
+        }
+    
+      ]).then(data => {
+        const manager = new Manager(data.managerName, data.managerId, data.managerEmail, data.managerNumber);
+        team.push(manager);
+        buildTeam();
+      });
+    }
 
     function engineer() {
         inquirer.prompt([
@@ -80,7 +79,7 @@ function manager() {
           {
             type: "input",
             name: "engineerId",
-            message: "What is the engineer's employee ID number?" 
+            message: "What is the engineer's ID number?" 
           },
     
           {
@@ -115,7 +114,7 @@ function manager() {
           {
             type: "input",
             name: "internId",
-            message: "What is the intern's employee ID number?" 
+            message: "What is the intern's ID number?" 
           },
     
           {
@@ -139,11 +138,11 @@ function manager() {
       }
   
       function buildSite () {
-        //TODO: throw 'team' into the output file 
+        fs.writeFileSync('./dist/team.html', createTeam(team), "UTF-8")
     
     }
 
     buildTeam();
-  }
+}
 
 run();
